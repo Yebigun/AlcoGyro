cecf2b8a6cab119ded39ebf9af090ed12b460b89
Yebigun <grzechu982@gmail.com>
2022-10-08 22:44:10 +0200

    saturday state

--- app/src/main/java/com/example/alcogyro/MainActivity.java
+++ app/src/main/java/com/example/alcogyro/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.alcogyro;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,7 +24,15 @@ import com.example.alcogyro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SensorManager sensorManager;
     private ActivityMainBinding binding;
+    private TabLayout tabs;
+    private FloatingActionButton fab;
+    private ViewPager viewPager;
+    private SectionsPagerAdapter sectionsPagerAdapter;
+    private Sensor gyroscopeSensor;
+    private SensorEventListener gyroscopeEventListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +41,60 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
+        sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        configureView();
+
     }
+
+    void configureView(){
+        bindObjects();
+        bindDetails();
+    }
+
+
+    void bindObjects(){
+        fab = binding.fab;
+        tabs = binding.tabs;
+        viewPager = binding.viewPager;
+    }
+
+    @SuppressLint("ServiceCast")
+    void bindDetails(){
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabs.setupWithViewPager(viewPager);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        gyroscopeEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if( sensorEvent.values[2] > 0.5f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (sensorEvent.values[2] < -0.5f){
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
